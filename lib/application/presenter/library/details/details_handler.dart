f6b596470733fc39d4baa433dcd7f69180d273cb
Zé Gabriel <98681112+KidGbzin@users.noreply.github.com>
2025-02-21 14:06:00 -0300

    🧩 #7 • Added background music player feature.
    - Integrated the "audioplayers" dependency.
    - Implemented an audio player in the Details view to play the game's theme music in the background.
    - Updated and improved related documentation.

--- lib/application/presenter/library/details/details_handler.dart
+++ lib/application/presenter/library/details/details_handler.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
