0b50755359703d3b5fef953385d3949127dca8a8
Zé Gabriel <98681112+KidGbzin@users.noreply.github.com>
2025-02-23 00:57:08 -0300

    🌐 #9 • Added support for Bahasa Indonesia language.
    - Added Bahasa Indonesia localization.
    - Removed an unused function in the L10nEnumeration class.

--- lib/application/core/enumerations/l10n_enumeration.dart
+++ lib/application/core/enumerations/l10n_enumeration.dart
@@ -1,7 +1,5 @@
 import 'dart:ui';
 
-import '../enumerations/logger_enumeration.dart';
-
 /// An enumeration of supported locales in the application.
 ///
 /// Each locale is represented by its language code and country code.
@@ -14,12 +12,24 @@ enum L10nEnumeration {
     locale: Locale('pt','BR'),
   ),
 
+  bahasaIndonesia(
+    iconPath: 'assets/flags/ID.gif',
+    label: 'BAHASA IMDONESIA',
+    locale: Locale('id', 'ID'),
+  ),
+
   /// English locale.
   english(
     iconPath: 'assets/flags/US.gif',
     label: 'ENGLISH',
     locale: Locale('en', 'US'),
   );
+
+  const L10nEnumeration({
+    required this.iconPath,
+    required this.locale,
+    required this.label,   
+  });
 
   /// The path to the flag icon for the locale.
   final String iconPath;
@@ -29,33 +39,4 @@ enum L10nEnumeration {
 
   /// The [Locale] object representing the locale.
   final Locale locale;
-
-  /// Creates a new [L10nEnumeration] with the given properties.
-  const L10nEnumeration({
-    required this.iconPath,
-    required this.locale,
-    required this.label,
-    
-  });
-
-  /// Returns the [L10nEnumeration] corresponding to the given language code.
-  ///
-  /// If the language code is not recognized, it returns the [english] locale by default.
-  static L10nEnumeration fromCode(String code) {
-    final Map<String, L10nEnumeration> table = <String, L10nEnumeration> {
-      'BR': L10nEnumeration.brazilianPortuguese,
-      'US': L10nEnumeration.english,
-    };
-
-    final L10nEnumeration? locale = table[code];
-
-    if (locale != null) return locale;
-
-    Logger.error.print(
-      message: 'Locale "$code" not found, defaulting to "US" locale.',
-      label: 'L10n Enumeration | From Code',
-    );
-
-    return L10nEnumeration.english;
-  }
 }
