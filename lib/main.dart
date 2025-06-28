961a341bf4cb9d22a6949c060a01e7ebf94bd335
Zé Gabriel <98681112+KidGbzin@users.noreply.github.com>
2025-06-28 18:01:59 -0300

    🧱 #76 • Integrate real-time error reporting with Crashlytics.
    - Added firebase_crashlytics as a dependency.
    - Integrated Crashlytics to automatically report all uncaught errors.
    - Ensure to log any future handled errors manually when needed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
@@ -22,92 +25,98 @@ import '../application.dart';
 import '../logger.dart';
 
 Future<void> main() async {
+  runZonedGuarded(() async {
 
-  // MARK: Initialization ⮟
+    // MARK: Initialization ⮟
 
-  WidgetsFlutterBinding.ensureInitialized();
+    WidgetsFlutterBinding.ensureInitialized();
 
-  setLocaleMessages('pt', PtBrMessages());
-  setLocaleMessages('id', IdMessages());
-  setLocaleMessages('en', EnMessages());
+    setLocaleMessages('pt', PtBrMessages());
+    setLocaleMessages('id', IdMessages());
+    setLocaleMessages('en', EnMessages());
 
-  await Logger.initialize();
+    await Logger.initialize();
 
-  Logger.start("The application has been started!");
-  Logger.start("Initializing the Firebase enviroment...");
+    Logger.start("The application has been started!");
+    Logger.start("Initializing the Firebase enviroment...");
 
-  await Firebase.initializeApp(
-    options: FirebaseOptions(
-      apiKey: const String.fromEnvironment("GOOGLE_API_KEY"),
-      appId: "1:923401966131:android:74b789539ded6e8e55c946",
-      messagingSenderId: "923401966131",
-      projectId: "midlet-store-52168",
-      storageBucket: "midlet-store-52168.firebasestorage.app",
-    ),
-  );
-
-  // MARK: Provider Instances ⮟
-
-  final ActivityService sActivity = ActivityService();
-  final AndroidService sAndroid = AndroidService();
-  final GoogleOAuthService sGoogleOAuth = GoogleOAuthService(const String.fromEnvironment('GOOGLE_SERVER_CLIENT'));
-
-  final AdMobService sAdMob = AdMobService(
-    bannerRectangleUnit: const String.fromEnvironment("ADMOB_BANNER_RECTANGLE"),
-    bannerUnit: const String.fromEnvironment("ADMOB_BANNER"),
-  );
-
-  final GitHubService sGitHub = GitHubService(
-    client: http.Client(),
-    token: const String.fromEnvironment("GITHUB_BUCKET_TOKEN"),
-  );
-
-  final SupabaseService sSupabase = SupabaseService(
-    anonKey: const String.fromEnvironment("SUPABASE_KEY"),
-    url: const String.fromEnvironment("SUPABASE_URL"),
-  );
-
-  final BucketRepository rBucket = BucketRepository(
-    sAndroid: sAndroid,
-    sGitHub: sGitHub,
-  );
-
-  final HiveRepository rHive = HiveRepository(sGitHub);
-  final SupabaseRepository rSupabase = SupabaseRepository(sSupabase);
-  final FirebaseMessagingService sFirebaseMessaging = FirebaseMessagingService(rSupabase);
-
-  // MARK: Run Application ⮟
-
-  runApp(MultiProvider(
-    providers: <SingleChildWidget> [
-      Provider<ActivityService>.value(
-        value: sActivity,
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: const String.fromEnvironment("GOOGLE_API_KEY"),
+        appId: "1:923401966131:android:74b789539ded6e8e55c946",
+        messagingSenderId: "923401966131",
+        projectId: "midlet-store-52168",
+        storageBucket: "midlet-store-52168.firebasestorage.app",
       ),
-      Provider<AdMobService>.value(
-        value: sAdMob,
-      ),
-      Provider<FirebaseMessagingService>.value(
-        value: sFirebaseMessaging,
-      ),
-      Provider<GoogleOAuthService>.value(
-        value: sGoogleOAuth,
-      ),
-      Provider<GitHubService>.value(
-        value: sGitHub,
-      ),
-      Provider<SupabaseService>.value(
-        value: sSupabase,
-      ),
-      Provider<BucketRepository>.value(
-        value: rBucket,
-      ),
-      Provider<HiveRepository>.value(
-        value: rHive,
-      ),
-      Provider<SupabaseRepository>.value(
-        value: rSupabase,
-      ),
-    ],
-    child: Application(),
-  ));
+    );
+
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+    // MARK: Provider Instances ⮟
+
+    final ActivityService sActivity = ActivityService();
+    final AndroidService sAndroid = AndroidService();
+    final GoogleOAuthService sGoogleOAuth = GoogleOAuthService(const String.fromEnvironment('GOOGLE_SERVER_CLIENT'));
+
+    final AdMobService sAdMob = AdMobService(
+      bannerRectangleUnit: const String.fromEnvironment("ADMOB_BANNER_RECTANGLE"),
+      bannerUnit: const String.fromEnvironment("ADMOB_BANNER"),
+    );
+
+    final GitHubService sGitHub = GitHubService(
+      client: http.Client(),
+      token: const String.fromEnvironment("GITHUB_BUCKET_TOKEN"),
+    );
+
+    final SupabaseService sSupabase = SupabaseService(
+      anonKey: const String.fromEnvironment("SUPABASE_KEY"),
+      url: const String.fromEnvironment("SUPABASE_URL"),
+    );
+
+    final BucketRepository rBucket = BucketRepository(
+      sAndroid: sAndroid,
+      sGitHub: sGitHub,
+    );
+
+    final HiveRepository rHive = HiveRepository(sGitHub);
+    final SupabaseRepository rSupabase = SupabaseRepository(sSupabase);
+    final FirebaseMessagingService sFirebaseMessaging = FirebaseMessagingService(rSupabase);
+
+    // MARK: Run Application ⮟
+    runApp(MultiProvider(
+      providers: <SingleChildWidget> [
+        Provider<ActivityService>.value(
+          value: sActivity,
+        ),
+        Provider<AdMobService>.value(
+          value: sAdMob,
+        ),
+        Provider<FirebaseMessagingService>.value(
+          value: sFirebaseMessaging,
+        ),
+        Provider<GoogleOAuthService>.value(
+          value: sGoogleOAuth,
+        ),
+        Provider<GitHubService>.value(
+          value: sGitHub,
+        ),
+        Provider<SupabaseService>.value(
+          value: sSupabase,
+        ),
+        Provider<BucketRepository>.value(
+          value: rBucket,
+        ),
+        Provider<HiveRepository>.value(
+          value: rHive,
+        ),
+        Provider<SupabaseRepository>.value(
+          value: rSupabase,
+        ),
+      ],
+      child: Application(),
+    ));
+  },
+  (error, stackTrace) {
+    FirebaseCrashlytics.instance.recordError(error, stackTrace);
+  });
 }
